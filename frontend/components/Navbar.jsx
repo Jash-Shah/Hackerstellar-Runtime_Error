9cc68753f0c4d6df2c58a9f4547f8ce5af6423cb
Akshay <afalke007@gmail.com>
2023-04-16 01:42:08 +0530

    navbar color change

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -47,8 +47,8 @@ function Navbar() {
 
     return (
         <>
-            <nav className="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 sticky w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
-                <div className="container flex flex-wrap items-center justify-between mx-auto">
+            <nav className="bg-slate-100 shadow-lg px-2 sm:px-4 py-2.5 dark:bg-gray-900 sticky w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+                <div className="container  flex flex-wrap items-center justify-between mx-auto">
                     <div className="flex">
                         <img
                             src="favicon.ico"
