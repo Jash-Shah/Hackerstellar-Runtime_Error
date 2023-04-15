7fa7df1d8fef9ca8cb7d2d0da4c8d6a9363818ad
Akshay <afalke007@gmail.com>
2023-04-16 00:49:40 +0530

    merge branch

--- frontend/pages/executeCustomer/index.js
+++ frontend/pages/executeCustomer/index.js
@@ -5,12 +5,12 @@ import ExecuteCustomerForm from "../../components/ExecuteCustomerForm";
 
 const App = () => {
     return (
-        <>
+        <div className="dark:bg-gray-900 bg-white">
             <Head>
                 <title>Execute Customer</title>
             </Head>
             <ExecuteCustomerForm></ExecuteCustomerForm>
-        </>
+        </div>
     );
 };
 
