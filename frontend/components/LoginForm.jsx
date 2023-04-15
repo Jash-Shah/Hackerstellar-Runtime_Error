6f526ac116521d35a33004ba4369e8204d0b7d8e
Jash Shah <jash28582@gmail.com>
2023-04-16 04:35:16 +0530

    Changed text in login form and service logos to turn white in dark mode

--- frontend/components/LoginForm.jsx
+++ frontend/components/LoginForm.jsx
@@ -50,7 +50,7 @@ function LoginForm() {
                 <div>
                     <Form.Item
                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                        label="Username"
+                        label={<label className="dark:text-white">Username</label>}
                         name="username"
                         rules={[
                             {
@@ -64,7 +64,7 @@ function LoginForm() {
                 <div>
                     <Form.Item
                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                        label="Password"
+                        label={<label className="dark:text-white">Password</label>}
                         name="password"
                         rules={[
                             {
