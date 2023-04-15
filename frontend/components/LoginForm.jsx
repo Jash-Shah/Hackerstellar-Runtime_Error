ba69a229c204e7c4454a97cd9c15093004f51582
toshan-luktuke <toshan1603@gmail.com>
2023-04-15 14:03:22 +0530

    Add login form to homepage and improve services

--- frontend/components/LoginForm.jsx
+++ frontend/components/LoginForm.jsx
@@ -40,7 +40,7 @@ function LoginForm () {
                 onFinish={onFinish}
                 autoComplete="off">
                 <div className="flex">
-                    <h5 class="m-auto mb-4 text-xl font-medium text-gray-900 dark:text-white">Login to the System</h5>
+                    <h5 class="m-auto mb-4 text-xl font-medium text-gray-900 dark:text-white">Login to VeriTrack</h5>
                 </div>
                 <div>
                     <Form.Item
@@ -74,7 +74,7 @@ function LoginForm () {
                     <div class="flex items-start mb-4">
                         <div class="flex items-start">
                             <div class="flex items-center h-5">
-                                <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
+                                <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"/>
                             </div>
                             <label for="remember" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
                         </div>
